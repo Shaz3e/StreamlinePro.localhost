72bb4ebdca6a97a1f7e2192364645ab510179861
Shahrukh A. Khan <iam@shaz3e.com>
2024-05-14 04:32:45 +0500

    Update create and edit todo views: add Select2 library for select elements and update TinyMCE configuration
    The changes include:
    
    * In `resources/views/admin/todo/create.blade.php` and `

--- resources/views/admin/todo/edit.blade.php
+++ resources/views/admin/todo/edit.blade.php
@@ -33,7 +33,7 @@
                             <div class="col-md-2">
                                 <div class="form-group">
                                     <label for="todo_label_id">Label</label>
-                                    <select name="todo_label_id" class="form-control" id="todo_label_id">
+                                    <select name="todo_label_id" class="form-control select2" id="todo_label_id">
                                         @foreach ($todoLabels as $label)
                                             <option value="{{ $label->id }}"
                                                 {{ $todo->todo_label_id == $label->id ? 'selected' : '' }}>
@@ -90,55 +90,63 @@
 @endsection
 
 @push('styles')
+
+<link href="{{ asset('assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
 @endpush
 
 @push('scripts')
+    <script src="{{ asset('assets/libs/select2/js/select2.min.js') }}"></script>
     <!--tinymce js-->
     <script src="{{ asset('assets/libs/tinymce/tinymce.min.js') }}"></script>
 
     <!-- init js -->
     <script>
-        // $(document).ready(function() {
-        //     0 < $("#todo_details").length && tinymce.init({
-        //         selector: "textarea#todo_details",
-        //         height: 300,
-        //         plugins: [
-        //             "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-        //             "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-        //             "save table contextmenu directionality emoticons template paste textcolor"
-        //         ],
-        //         toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
-        //         style_formats: [{
-        //             title: "Bold text",
-        //             inline: "b"
-        //         }, {
-        //             title: "Red text",
-        //             inline: "span",
-        //             styles: {
-        //                 color: "#ff0000"
-        //             }
-        //         }, {
-        //             title: "Red header",
-        //             block: "h1",
-        //             styles: {
-        //                 color: "#ff0000"
-        //             }
-        //         }, {
-        //             title: "Example 1",
-        //             inline: "span",
-        //             classes: "example1"
-        //         }, {
-        //             title: "Example 2",
-        //             inline: "span",
-        //             classes: "example2"
-        //         }, {
-        //             title: "Table styles"
-        //         }, {
-        //             title: "Table row 1",
-        //             selector: "tr",
-        //             classes: "tablerow1"
-        //         }]
-        //     })
-        // });
+        $(document).ready(function() {
+            // init select2
+            $(".select2").select2();
+
+            0 < $("#todo_details").length && tinymce.init({
+                selector: "textarea#todo_details",
+                height: 300,
+                plugins: [
+                    "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+                    "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                    "save table directionality emoticons template paste"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+                style_formats: [{
+                        title: "Bold text",
+                        inline: "b"
+                    },
+                    {
+                        title: "Red text",
+                        inline: "span",
+                        styles: {
+                            color: "#ff0000"
+                        }
+                    }, {
+                        title: "Red header",
+                        block: "h1",
+                        styles: {
+                            color: "#ff0000"
+                        }
+                    }, {
+                        title: "Example 1",
+                        inline: "span",
+                        classes: "example1"
+                    }, {
+                        title: "Example 2",
+                        inline: "span",
+                        classes: "example2"
+                    }, {
+                        title: "Table styles"
+                    }, {
+                        title: "Table row 1",
+                        selector: "tr",
+                        classes: "tablerow1"
+                    }
+                ]
+            })
+        });
     </script>
 @endpush
