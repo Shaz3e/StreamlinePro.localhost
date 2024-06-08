2060c451780c1b0bc984dd428138cc0bd41b3c01
Shahrukh A. Khan <iam@shaz3e.com>
2024-06-09 02:51:11 +0500

    Added link to 'icons.min.css' in app.blade.php layout.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <!-- Bootstrap Css -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/fonts/remixicon.min.css') }}" rel="stylesheet" type="text/css" />
 
     @stack('styles')
