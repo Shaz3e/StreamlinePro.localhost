628218d5b401e62ef07172021953edd73e5da1b0
Shahrukh A. Khan <iam@shaz3e.com>
2024-06-22 16:14:01 +0500

    "Moved payment methods section to a separate include file and rearranged layout in user invoice show blade template."

---
+++ resources/views/user/invoice/payment-methods.blade copy.php
@@ -0,0 +1,50 @@
+{{-- Payment Methods --}}
+@if ($invoice->total !== $invoice->total_paid)
+    <ul class="list-inline mb-0">
+
+        {{-- Stripe Payments --}}
+        @if (DiligentCreators('stripe') == 1)
+            <li class="list-inline-item">
+                <button type="button" class="btn btn-dark waves-effect waves-light"
+                    data-bs-toggle="modal" data-bs-target="#addStripePayment">
+                    {{ DiligentCreators('stripe_display_name') }}
+                </button>
+                @include('user.invoice.stripe.form')
+            </li>
+        @endif
+        @if (DiligentCreators('stripe_hosted_checkout') == 1)
+            <li class="list-inline-item">
+                <form action="{{ route('payment-method.stripe.hosted.checkout') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="invoice_id" value="{{ $invoice->id }}">
+                    <button type="submit" class="btn btn-dark waves-effect waves-light">
+                        {{ DiligentCreators('stripe_hosted_checkout_display_name') }}
+                    </button>
+                </form>
+            </li>
+        @endif
+
+        {{-- N-Genius Network Payment --}}
+        @if (DiligentCreators('ngenius') == 1)
+            <li class="list-inline-item">
+                <button type="button" class="btn btn-dark waves-effect waves-light"
+                    data-bs-toggle="modal" data-bs-target="#addNGeniusNetworkPayment">
+                    {{ DiligentCreators('ngenius_display_name') }}
+                </button>
+                @include('user.invoice.ngenius-network.form')
+            </li>
+        @endif
+        @if (DiligentCreators('ngenius_hosted_checkout') == 1)
+            <li class="list-inline-item">
+                <form action="{{ route('payment-method.ngenius-network.hosted.checkout') }}"
+                    method="POST">
+                    @csrf
+                    <input type="hidden" name="invoice_id" value="{{ $invoice->id }}">
+                    <button type="submit" class="btn btn-dark waves-effect waves-light">
+                        {{ DiligentCreators('ngenius_hosted_checkout_display_name') }}
+                    </button>
+                </form>
+            </li>
+        @endif
+    </ul>
+@endif
