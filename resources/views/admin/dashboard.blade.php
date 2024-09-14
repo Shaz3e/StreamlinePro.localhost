acf86fd90497b4e0e8fb9831b17fd2588e95d344
Shahrukh Akhtar <iam@shaz3e.com>
2024-09-14 18:28:46 +0500

    "Removed welcome message from admin dashboard"

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,6 @@
 @extends('components.layouts.app')
 
 @section('content')
-    <div class="row mb-3">
-        <div class="col-md-12">
-            Welcome Back <strong>{{ auth()->user()->name }}</strong>
-        </div>
-    </div>
     @include('admin.dashboard.dashboard')
 @endsection
 
